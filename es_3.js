473d9329cf99456a59b4177aaf1287b778f1daaa
Simona7-code <61553187+Simona7-code@users.noreply.github.com>
2023-03-08 15:48:20 +0100

    1 file modified, 2 added

---
+++ es_3.js
@@ -0,0 +1,8 @@
+/*A partire dal codice realizzato nell’esercizio precedente realizzate
+una classe "archivio" contenente un metodo che restituisce un
+array di libri che corrispondono ad una certa stringa.
+L’array in cui eseguire la ricerca è caricato in una variabile nella
+stassa classe tramite il costruttore.
+Il singolo libro può essere a sua volta rappresentato da una classe
+Libro con i quattro attributi necessari, specificati nella consegna
+del progetto.*/
