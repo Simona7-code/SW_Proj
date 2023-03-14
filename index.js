695bd2052c79bcf258006aef0e983d42e525b8af
Simona7-code <61553187+Simona7-code@users.noreply.github.com>
2023-03-14 11:51:52 +0100

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -19,61 +19,27 @@ il titolo
 il nominativo dell'utente a cui è stato prestato*/
 
 
-class Libro {
-  constructor(loc, tit, aut, loan_client) {
+class Archivio {
+  constructor() {
+    const Arch = [];
+  }
+  add(loc, tit, aut, loan_client) {
+    //const Arch = []; #TODO COME FACCIO A RENDERLO VISIBILE LASCIANDOLO NEL CONSTRUCTOR?
     this.tit = tit;
     this.aut = aut;
     this.loc = loc;
     this.loan_client = loan_client;
-  }
-}
-
-class Archivio {
-  add() {
      //COSTRUTTORE DI OGGETTI CLASSE LIBRO 
-
-    /*const libro1 = new Libro("x568l",'Alice in Wonderland', "Lewis Carol", "G_G_6X" );
-    const libro2 = new Libro("x598l",'TIT', "aUT", "G_s_6X");
-    const libro3 = new Libro("x798l",'TIT1', "aUT1", "G_6_6X");
-    const libro4 = new Libro("x398l",'TIT2', "aUT2", "G_9_6X");
-    const libro5 = new Libro("x298l",'TIT3', "aUT3", "G_0_6X");
-    console.log(libro5.tit)*/
+    let New_doc= [this.tit,this.aut = aut,this.loc, this.loan_client];
+    //NON PUSHAVA PERCHè LASCIANDOLO QUA OGNI VOLTA LO REINIZIALIZZI A ZERO
+    Arch.push(New_doc)
   }
 
-  cerca(titolo_cerca){
+  /*cerca(titolo_cerca){
 
-    //creo nuovo array vuoto in cui salverà titolo e autore libro
-    let libri_trovati = []
-
- /*   //per ogni oggetto libro in array libreria
-    for (let libro of libreria){
-
-
-      // COME ITERARE SU ATTRIBUTO (TITOLO) DI TUTTI GLI OGGETTI DENTRO OGGETTO (LIBRI IN ARCHIVIO)???????????______________________________________________________________
-    
-      //in let titolo corrente ci salvo il titolo del libro corrente nel ciclo
-      let titolo_corr = libro["titolo"]
-
-      //Log di check per vedere se funzia e i valori nelle variabili sono i voluti
-      console.log( "titolo corrente->", titolo_corr)
-      console.log( "vedi se lo trova->", titolo_corr.toLowerCase().includes(titolo_cerca.toLowerCase())? "trovato":"non trovato");
-    
-      //se la versione lowercase del titolo corrente include la versione lowercase del titolo cercato
-      //incremento var trovato e faccio push del titolo e autore 
-      if (titolo_corr.toLowerCase().includes(titolo_cerca.toLowerCase())=== true) {
-        trovato++
-        libri_trovati.push([libro["titolo"] + ", Scritto da " + libro["autore"]])
-
-        //Log di check per vedere se funzia e i valori nelle variabili sono i voluti
-        console.log("vedi se aggiorna conta-->",trovato)
-        console.log("vedi se aggiunge libri trovati ad array->", libri_trovati)
-      } 
-    }*/
-
-  }   
+  }  */ 
 
 }
-
 
 
 //prova con div--> OUTPUT: 2 
@@ -81,7 +47,11 @@ class Archivio {
 let titolo_cerca = "TIT";
 
 const Archivio_Pisa = new Archivio();
-Archivio_Pisa.add();
-Archivio_Pisa.cerca(titolo_cerca);
-
+Archivio_Pisa.add("x568l",'Alice in Wonderland', "Lewis Carol", "G_G_6X");
+Archivio_Pisa.add("x798l",'TIT1', "aUT1", "G_6_6X");
+Archivio_Pisa.add("x398l",'TIT2', "aUT2", "G_9_6X");
+Archivio_Pisa.add("x598l",'TIT', "aUT", "G_s_6X");
+Archivio_Pisa.add("x298l",'TIT3', "aUT3", "G_0_6X");
+//Archivio_Pisa.cerca(titolo_cerca);
+console.log(Archivio_Pisa)
 //console.log(libro5.tit)
